c734665ceb2b0f2633e7df70757f269ad4ee5593
Jasmine Soong <soong.jasmine@gmail.com>
2016-10-11 18:33:33 -1000

    incomplete

--- functions.js
+++ functions.js
@@ -23,7 +23,7 @@ function flipSwitch(){
   }
 }
 /*For loops
-Sometimes we want to loop over a collection of data stored in an Arrayor inspect each character in a String. Or we just want to execute a block of code a set number of times before continuing on with the execution of our program. For loops allow us to execute a block of code and at the end of each iteration of our for loop we will evaluate a condition to determine if our loop should run again similar to our if statment.
+Sometimes we want to loop over a collection of data stored in an Array or inspect each character in a String. Or we just want to execute a block of code a set number of times before continuing on with the execution of our program. For loops allow us to execute a block of code and at the end of each iteration of our for loop we will evaluate a condition to determine if our loop should run again similar to our if statment.
 
 Our for loop is made up of 3 parts: 1. Variable initialization 2. Termination condition 3. Afterthought
 
@@ -48,62 +48,150 @@ Function - youGetTaco
 Write a function that takes a single String parameter called action. If the value passed into our function is eat have the function return the String value EAT TACOS.
 */
 
-
+var action = "eat";
+function youGetTaco(action){
+  if (action === "eat"){
+    return "EAT TACOS";
+  }
+}
+console.log('youGetTaco: ', youGetTaco(action))
 
 /*Function - isNumberGreaterThan
 Write a function that takes two variables of type Number called first and second respectively. Return true if the first number is greater than the second.*/
 
-
+function isNumberGreaterThan(first, second){
+  if (first > second){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+console.log('isNumberGreaterThan A: ', isNumberGreaterThan(10, 2));
+console.log('isNumberGreaterThan B: ', isNumberGreaterThan(2, 10));
 
 /*Function - isTrue
 Write a function that takes in a Boolean value named val and have the function return whether the value is true.*/
 
-
-
+function isTrue(val){
+  if (val === "takoyaki"){
+    return true;
+  }
+}
+console.log('isTrue: ', isTrue("takoyaki"));
 
 /*Function - isFalse
 Write a function that takes in a Boolean value named val and have the function return whether the value is false.*/
 
-
-
+function isFalse(val){
+    if (val === "mochi"){
+      return false;
+    }
+}
+console.log('isFalse: ', isFalse("mochi"));
 
 /*Function - isEqual
 Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are equal otherwise return 'Y U NO MATCH!'.*/
 
+function isEqual(firstWord, secondWord){
+  if (firstWord === secondWord){
+    return "AWWWWRIGHT";
+  }
+  else {
+    return "Y U NO MATCH!";
+  }
+}
+console.log('isEqual A: ', isEqual("takoyaki", "mochi"));
+console.log('isEqual B: ', isEqual("takoyaki", "takoyaki"));
 
 /*Function - isNotEqual
 Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are not equal otherwise return 'Y U MATCH!'.*/
 
+function isNotEqual(firstWord, secondWord){
+  if (firstWord !== secondWord){
+    return "AWWWWRIGHT";
+  }
+  else {
+    return "Y U NO MATCH!";
+  }
+}
+console.log('isNotEqual A: ', isNotEqual("takoyaki", "mochi"));
+console.log('isNotEqual B: ', isNotEqual("mochi", "mochi"));
 
 /*Function - doubleEquals
 Write a function that takes two variables of type Boolean called first and second respectively. Return true if both values are true otherwise return false.*/
 
-
+function doubleEquals(first, second){
+  if (first && second === true){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+console.log('doubleEquals A: ', doubleEquals(15>1, 20>5));
+console.log('doubleEquals B: ', doubleEquals("takoyaki", "mochi"));
 
 /*Function - totalOver30
 Write a function that takes three variables of type Number called first and second and third respectively. Return true if the sum of all values are greater than 30 otherwise return false.*/
 
-
+function totalOver30(first, second, third){
+  if (first + second + third > 30){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+console.log('totalOver30 A: ', totalOver30(5, 10, 20));
+console.log('totalOver30 B: ', totalOver30(2, 3, 5));
 
 /*Function - totalUnderWhat
 Write a function that takes four variables of type Number called first and second and third and fourth respectively. Return true if the sum of first, second and third are less than fourth otherwise return false.*/
 
-
+function totalUnderWhat(first, second, third, fourth){
+  if (first + second + third < fourth){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+console.log('totalUnderWhat A: ', totalUnderWhat(2, 4, 6, 20))
+console.log('totalUnderWhat B: ', totalUnderWhat(5, 10, 15, 6))
 
 /*Function - looptoNumber
 Write a function that takes in a Number value named limit and have the function write a for loop that loops the number of times of limit and console.log each number as the loop executes.*/
 
-
+function looptoNumber(limit){
+  for (var i = 0; i<limit; i++){
+    console.log(i);
+  }
+}
+looptoNumber(5)
 
 /*Function - showEachValue
 Write a function that takes in a Array value named characters that has a sequence of single character String values and have the function write a for loop that loops for each number of elements in the Array and console.log each character as the loop executes.*/
 
+var alphabet = ["A", "B", "C", "D"];
 
+function showEachValue(characters){
+  for(var i = 0; i<characters.length; i++){
+    console.log(characters[i]);
+  }
+}
+showEachValue(alphabet)
 
 /*Function - createArrayFromString
 Write a function that takes a single variable of type String called word and write a for loop that creates an Array made up of each character in word except for "A" or "a". We don't want no stinking "A" or "a" in our Array. Note: You will need to use the Array.push() method to complete this function.*/
 
 
+function createArrayFromString(word){
+  for(var i = 0; i<word.length; i++){
+    
+  }
+}
+createArrayFromString()
 
 /*Function - greatSummator
 Write a function that takes an Array with any number of type Number and write a for loop to add all numbers in the Array and return the sum.*/
